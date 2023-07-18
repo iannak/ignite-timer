26b5ed491aa0a0650d6c9fd6ea59cd16d9a2e835
annakarolinafranz <annakarolinafranz91@outlook.com>
2023-07-17 22:15:00 -0300

    styles status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,19 +20,25 @@ export function History() {
               <td>Tarefa</td>
               <td>00:00:00</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>00:00:00</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>00:00:00</td>
               <td>Há 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
