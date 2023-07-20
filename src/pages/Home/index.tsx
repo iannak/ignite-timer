745b34a372215c65c73e0df8ece492bd64d88abb
annakarolinafranz <annakarolinafranz@pop-os.localdomain>
2023-07-19 22:14:48 -0300

    settings react-hook-form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 import {
   CountdownContainer,
   FormContainer,
@@ -8,18 +10,20 @@ import {
   StartCountdonwButton,
   TaskInput,
 } from "./styles";
-import { useState } from "react";
 
 export function Home() {
-  const [task, setTask] = useState("");
+  const { register, handleSubmit, watch } = useForm();
 
-  function resetForm(){
-    setTask("")
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
   }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -27,8 +31,9 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para seu projeto"
-            onChange={(e) => setTask(e.target.value)}
+            // onChange={(e) => setTask(e.target.value)}
             value={task}
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -45,6 +50,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -59,7 +65,7 @@ export function Home() {
         <span>0</span>
       </CountdownContainer>
 
-      <StartCountdonwButton disabled={!task} type="submit">
+      <StartCountdonwButton disabled={isSubmitDisabled} type="submit">
         <Play size={24} />
         Começar
       </StartCountdonwButton>
